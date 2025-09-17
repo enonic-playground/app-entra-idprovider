c6d71f88a9e09a1970b3364ed9b884ef6e4b15f0
Olav Landsverk <stud-oll@ssb.no>
2025-09-17 09:17:41 +0200

    feat: paste group functions from azure-ad-id-provider

---
+++ src/main/resources/lib/group.js
@@ -0,0 +1,219 @@
+exports.createAndUpdateGroupsFromJwt = function(params) {
+    var idProviderConfig = getIdProviderConfig();
+    log.info('idProviderConfig:' + toStr(idProviderConfig));
+
+    var createAndUpdateGroupsOnLoginFromGraphApi = !!idProviderConfig.createAndUpdateGroupsOnLoginFromGraphApi;
+    log.info('createAndUpdateGroupsOnLoginFromGraphApi:' + toStr(createAndUpdateGroupsOnLoginFromGraphApi));
+
+    if (createAndUpdateGroupsOnLoginFromGraphApi) {
+        return fromGraph(params);
+    }
+}; // createAndUpdateGroupsFromJwt
+
+// get groups from graph api
+function fromGraph(params) {
+    var idProviderConfig = getIdProviderConfig();
+    // https://docs.microsoft.com/en-us/graph/api/user-list-memberof?view=graph-rest-1.0&tabs=cs
+    // https://developer.microsoft.com/en-us/graph/graph-explorer?request=me/memberOf&method=GET&version=v1.0&GraphUrl=https://graph.microsoft.com
+
+    var pageSize = idProviderConfig.pageSize ? '?$top=' + idProviderConfig.pageSize : '';
+
+    var groupRequest = {
+        method: 'GET',
+        url: 'https://graph.microsoft.com/v1.0/users/' + params.jwt.payload.oid + '/memberOf' + pageSize,
+        headers: {
+            Accept: 'application/json',
+            Authorization: 'Bearer ' + params.accessToken
+        },
+        //proxy: idProviderConfig.proxy
+    };
+
+    var groupResponse = sendRequest(groupRequest);
+
+    var body = JSON.parse(groupResponse.body);
+    if (body && body.value) {
+        // find users current ad groups
+        var groupKeysInXp = getGroups(params.user.key)
+            .filter(function(group) {
+                return group.key.startsWith('group:' + params.user.idProvider + ':azure-ad-'); // Only groups from AD
+            })
+            .map(function(group) {
+                return group.key;
+            });
+
+        // create or modify groups and add the user to the group
+        var groups = body.value;
+
+        // filter groups
+        if(idProviderConfig.groupFilter) {
+            var groupFilters = forceArray(idProviderConfig.groupFilter);
+            var checkGroups = groupFilters.reduce((t, f) => {
+                f.regexp = new RegExp(f.regexp)
+                if(f.and === true || t[t.length -1].length === 0) {
+                    t[t.length -1].push(f);
+                } else{
+                    t.push([f]);
+                }
+                return t;
+            }, [[]])
+
+            log.info('groupFilters:' + toStr(checkGroups))
+
+            groups = groups.reduce((filteredGroups, group) => {
+                for(let i = 0; i < checkGroups.length; i++) {
+                    var checkGroup = checkGroups[i];
+                    var match = false;
+                    for(let j = 0; j < checkGroup.length; j++) {
+                        var filter = checkGroup[j];
+                        if(filter.regexp.test(group[filter.groupProperty])) {
+                            match = true;
+                        } else {
+                            match = false;
+                            break;
+                        }
+                    }
+                    if(match) {
+                        filteredGroups.push(group);
+                        break;
+                    }
+                }
+                return filteredGroups;
+            }, [])
+            log.info('groupsAfterFilter:' + toStr(groups));
+        }
+
+        var groupKeysinAd = [];
+        groups.forEach(function(adGroup) {
+            var xpGroup = createOrModify({
+                idProvider: params.user.idProvider,
+                name: sanitizeName('azure-ad-' + adGroup.id),
+                displayName: adGroup.displayName,
+                description: adGroup.description
+            });
+            groupKeysinAd.push(xpGroup.key);
+        });
+        log.info('groupKeysinAd:' + toStr(groupKeysinAd));
+
+        var newGroupKeys = inFirstButNotInSecond(groupKeysinAd, groupKeysInXp);
+        log.info('newGroupKeys:' + toStr(newGroupKeys));
+
+        var oldGroupKeys = inFirstButNotInSecond(groupKeysInXp, groupKeysinAd);
+        log.info('oldGroupKeys:' + toStr(oldGroupKeys));
+
+        newGroupKeys.forEach(function(groupKey) {
+            addUser({
+                groupKey: groupKey,
+                userKey: params.user.key
+            });
+        });
+
+        oldGroupKeys.forEach(function(groupKey) {
+            removeUser({
+                groupKey: groupKey,
+                userKey: params.user.key
+            });
+        });
+        return groupKeysinAd
+    } else {
+        log.info('Could not load and create groups on login, turn on debug to see more infomation');
+    }
+}
+
+function inFirstButNotInSecond(a1, a2) {
+	var a2obj = {};
+	a2.forEach(function(v2) {
+		a2obj[v2] = true;
+	});
+	return a1.filter(function(v1) {
+		return !a2obj.hasOwnProperty(v1);
+	});
+};
+
+function runAsAdmin(callback) {
+    return contextLib.run({
+        user: {
+            login: 'su',
+            idProvider: 'system'
+        },
+        principals: ["role:system.admin"]
+    }, callback);
+};
+
+function toStr(value) {
+	var replacer = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+	var space = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 4;
+	return JSON.stringify(value, replacer, space);
+};
+
+function sanitizeName(name) {
+	return name.toLowerCase()
+		.replace(/[!"()]+/g, '') // ASCII removed.
+		.replace(/[#$%&'*+,/:;<=>?@[\\\]^_`{|}~\s]+/g, '-') // ASCII replaced.
+		.replace(/[æÆ]/g, 'ae').replace(/[øØ]/g, 'o').replace(/[åÅ]/g, 'a') // Norwegian chars.
+		.replace(/[äÄ]/g, 'ae').replace(/[öÖ]/g, 'o') // Swedish chars.
+		.replace(/--+/g, '-') // Two or more dashes becomes just one.
+		.replace(/^[-.]+/, '') // Do not begin with - or .
+		.replace(/[-.]+$/, ''); // Do not end in - or .
+};
+
+function getGroups(principalKey) {
+	var principals = runAsAdmin(function() {
+		return getMemberships(principalKey);
+	});
+	log.info('getGroups(' + toStr(principalKey) + ') principals:' + toStr(principals));
+	var groups = principals.filter(function(principal) {
+		return principal.type === 'group';
+	});
+	log.info('getGroups(' + toStr(principalKey) + ') -->' + toStr(groups));
+	return groups;
+};
+
+
+function createOrModify(params) {
+    log.info('createOrModify(' + toStr(params) + ')');
+
+    var group = runAsAdmin(function() {
+        return getPrincipal('group:' + params.idProvider + ':' + params.name);
+    });
+    //log.info('getPrincipalResult:' + toStr(group));
+
+    if (group) {
+        if (group.displayName === params.displayName && group.description === params.description) {
+            log.info('unchanged group:' + toStr(group));
+        } else {
+            group = runAsAdmin(function() {
+                return modifyGroup({
+                    key: group.key,
+                    editor: function(c) {
+                        c.displayName = params.displayName;
+                        c.description = params.description || '';
+                        return c;
+                    }
+                });
+            });
+            log.info('modified group:' + toStr(group));
+        }
+    } else {
+        runAsAdmin(function() {
+            group = createGroup(params);
+        });
+        log.info('created group:' + toStr(group));
+    }
+    return group;
+} // function createOrModify
+
+function addUser(params) {
+    var addMembersResult = runAsAdmin(function() {
+        return addMembers(params.groupKey, [params.userKey]);
+    });
+    log.info('addMembersResult:' + toStr(addMembersResult)); // In Enonic XP 6.9.2 return undefined even if group is unmodified
+}
+
+
+function removeUser(params) {
+    log.info('removeUser(' + toStr(params) + ')');
+    var removeMembersResult = runAsAdmin(function() {
+        return removeMembers(params.groupKey, [params.userKey]);
+    });
+    log.info('removeMembersResult:' + toStr(removeMembersResult));
+}
